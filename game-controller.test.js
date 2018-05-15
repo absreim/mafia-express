0a6b7985d3278f020aa3af925a1dea6f33818273
Brook Li <absreim@users.noreply.github.com>
2018-05-15 01:46:47 -0400

    Several working tests. Nested to simulate sequential order.

--- game-controller.test.js
+++ game-controller.test.js
@@ -2,16 +2,44 @@ const GameController = require("./game-controller.js")
 const Shared = require("./shared.js")
 
 const totalPlayers = 3
-const numWerewolves = 1
-const gc = new GameController.GameController(totalPlayers, numWerewolves)
+const numWerewolves = 2
+var gc = null
 
-gc.joinPlayer("Alice")
-gc.joinPlayer("Bob")
-gc.joinPlayer("Cory")
+beforeEach(() => {
+    gc = new GameController.GameController(totalPlayers, numWerewolves)
+    gc.joinPlayer("Alice")
+    gc.joinPlayer("Bob")
+    gc.joinPlayer("Cory")
+})
 
 test("Alice, Bob, and Cory should be in the game.", () => {
     const playerNames = Object.keys(gc.gameState.players)
     expect(playerNames).toContain("Alice")
     expect(playerNames).toContain("Bob")
     expect(playerNames).toContain("Cory")
+})
+
+test("Number of werewolves should be automatically scaled down to 1.", () => {
+    const claimedNumWerewolves = gc.numWerewolves
+    const actualNumWerewolves = Object.keys(gc.gameState.players).filter(
+        player => gc.gameState.players[player].isWerewolf).length
+    expect(claimedNumWerewolves).toEqual(1)
+    expect(actualNumWerewolves).toEqual(1)
+    })
+
+test("Game should be in STARTED phase.", () => {
+        const phase = gc.gameState.phase
+        expect(phase).toEqual(Shared.Phases.STARTED)
+    })
+
+describe("After all players acknowledged STARTED phase.", () => {
+    beforeEach(() => {
+        gc.handleMessage({type: Shared.ClientMessageType.ACKNOWLEDGE},"Alice")
+        gc.handleMessage({type: Shared.ClientMessageType.ACKNOWLEDGE},"Bob")
+        gc.handleMessage({type: Shared.ClientMessageType.ACKNOWLEDGE},"Cory")
+    })
+    test("Game should be in NIGHTTIME phase.", () => {
+        const phase = gc.gameState.phase
+        expect(phase).toEqual(Shared.Phases.NIGHTTIME)
+    })
 })
